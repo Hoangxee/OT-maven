e753ccf202b9d921e65a020237cb8c0409527667
Hoangxee <hoangxe23@gmail.com>
2023-10-19 17:24:49 +0700

    change driver

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -44,12 +44,12 @@ public class BaseTest {
                     System.setProperty("webdriver.chrome.driver", "C:/Users/Admin/Downloads/Downloads/IntelliJ/project/OT-maven/browserDrivers/chromedriver.exe");
                     System.out.println("osName: Windows!");
                 }
-                ChromeOptions options = new ChromeOptions();
-                options.addArguments("headless");
-                options.addArguments("no-sandbox");
-                options.addArguments("window-size=1200x600");
-                driver = new ChromeDriver(options);
-//                driver = new ChromeDriver();
+//                ChromeOptions options = new ChromeOptions();
+//                options.addArguments("headless");
+//                options.addArguments("no-sandbox");
+//                options.addArguments("window-size=1200x600");
+//                driver = new ChromeDriver(options);
+                driver = new ChromeDriver();
                 break;
             case FIREFOX:
 //                driver = WebDriverManager.firefoxdriver().create();
