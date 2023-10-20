cb4fe2812db0d5c71894a85e837e28999d9a808a
Hoangxee <hoangxe23@gmail.com>
2023-10-20 14:06:20 +0700

    try to headless

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -46,12 +46,12 @@ public class BaseTest {
                     System.setProperty("webdriver.chrome.driver", projectPath+ "/browserDrivers/chromedriver.exe");
                     System.out.println("osName: Windows!");
                 }
-//                ChromeOptions options = new ChromeOptions();
-//                options.addArguments("headless");
-//                options.addArguments("no-sandbox");
-//                options.addArguments("window-size=1200x600");
-//                driver = new ChromeDriver(options);
-                driver = new ChromeDriver();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("headless");
+                options.addArguments("no-sandbox");
+                options.addArguments("window-size=1200x600");
+                driver = new ChromeDriver(options);
+//                driver = new ChromeDriver();
                 break;
             case FIREFOX:
 //                driver = WebDriverManager.firefoxdriver().create();
