87eac472c43e18d92f4c272e88f103c4504b3ed5
Hoangxee <hoangxe23@gmail.com>
2023-10-25 14:58:52 +0700

    test

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -56,8 +56,8 @@ public class BaseTest {
 
 
                 ChromeOptions options = new ChromeOptions();
-//                options.setBinary("/usr/bin/google-chrome");
-                options.setBinary("/usr/bin/google-chrome/chrome");
+                options.setBinary("/opt/google/chrome/chrome");
+//                options.setBinary("/usr/bin/google-chrome/chrome");
                 options.addArguments("start-maximized");
                 options.setExperimentalOption("excludeSwitches", Collections.singletonList("enable-automation"));
                 options.setExperimentalOption("useAutomationExtension", false);
