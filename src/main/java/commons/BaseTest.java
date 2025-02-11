e614e6bd19018fc315cd6a7a7a643f74770a2dc3
Hoangxee <hoangxe23@gmail.com>
2025-02-11 09:03:38 +0700

    add: gitlab ci

--- src/main/java/commons/BaseTest.java
+++ src/main/java/commons/BaseTest.java
@@ -95,9 +95,7 @@ public class BaseTest {
                 ChromeOptions headlessChromeOptions = new ChromeOptions();
                 headlessChromeOptions.addArguments("--no-sandbox");
                 headlessChromeOptions.addArguments("--headless");
-                headlessChromeOptions.addArguments("--window-size=1920,1080");
-//                headlessChromeOptions.addArguments(
-//                        "user-agent='Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.6943.53 Safari/537.36'");
+//                headlessChromeOptions.addArguments("--window-size=1920,1080");
                 headlessChromeOptions.addArguments("--disable-dev-shm-usage");
 
                 driver = new ChromeDriver(headlessChromeOptions);
